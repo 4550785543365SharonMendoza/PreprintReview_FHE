3a808ac8cb75c25372992732f0ed1c53004fdce7
4550785543365SharonMendoza <bogdan-l-570989639@rambler.ru>
2025-12-04 11:42:04 +0800

    add demo-database-router.java, validator-src-utils.scss, provider-message-decorator.ts

---
+++ interface-registry-module/serializer-email/simple-queries-configuration/demo-database-router.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\interface-registry-module\serializer-email\simple-queries-configuration\demo-database-router.java
+// Generated at: 2025-10-10T05:17:46.663Z
